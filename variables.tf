8606ac1a15ce7c453ee06675cbe798f38c123652
adegbolae <112221382+adegbolae@users.noreply.github.com>
2022-10-28 16:59:34 -0500

    updated vaiables

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJune2021Key" 
+    default     =  "myubundukey" 
 }
 
 variable "instance_type" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-020db2c14939a8efb" 
+    default     = "ami-0a59f0e26c55590e9" 
 }
 variable "versioning" {
     type        = bool
